14551da6bd314c53d0a1405e6342d728236568ba
pashutk <me@pashutk.ru>
2020-07-12 19:30:00 +0300

    Finish config.ts refac

--- sneakernews-releases/config.ts
+++ sneakernews-releases/config.ts
@@ -1,28 +1,36 @@
 import { config } from 'dotenv';
 import * as E from 'fp-ts/lib/Either';
 import * as RR from 'fp-ts/lib/ReadonlyRecord';
+import * as RE from 'fp-ts/lib/ReaderEither';
+import * as O from 'fp-ts/lib/Option';
 import { pipe, flow, identity } from 'fp-ts/lib/function';
-import { sequenceS } from 'fp-ts/lib/Apply';
 config();
 
-const getFromEnvStrict = (key: string) => (env: NodeJS.ProcessEnv) =>
-  pipe(
-    process.env[key],
-    E.fromNullable(new Error('No BOT_TOKEN process env specified'))
+const isString = (a: unknown): a is string => typeof a === 'string';
+
+class EnvNotSpecifiedError extends Error {
+  constructor(public readonly envKey: string) {
+    super(`No ${envKey} process env speciified`);
+  }
+}
+
+const getFromEnvStrict = (
+  key: string
+): RE.ReaderEither<NodeJS.ProcessEnv, EnvNotSpecifiedError, string> =>
+  flow(
+    RR.lookup(key),
+    O.filter(isString),
+    E.fromOption(() => new EnvNotSpecifiedError(key))
   );
 
-const seqSEither = sequenceS(E.either);
-
-export const getConfig = (env: NodeJS.ProcessEnv) =>
-  pipe(
-    {
-      BOT_TOKEN: getFromEnvStrict('BOT_TOKEN'),
-      DB_NAME: getFromEnvStrict('DB_NAME'),
-      GOOGLE_TIMEZONE_API_KEY: getFromEnvStrict('GOOGLE_TIMEZONE_API_KEY'),
-    },
-    RR.map((a) => a(env)),
-    seqSEither
-  );
+export const getConfig = pipe(
+  {
+    BOT_TOKEN: getFromEnvStrict('BOT_TOKEN'),
+    DB_NAME: getFromEnvStrict('DB_NAME'),
+    GOOGLE_TIMEZONE_API_KEY: getFromEnvStrict('GOOGLE_TIMEZONE_API_KEY'),
+  },
+  RR.sequence(RE.readerEither)
+);
 
 export const getConfigUnsafe = flow(
   getConfig,
