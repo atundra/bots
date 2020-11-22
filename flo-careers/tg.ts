bf060ff56eca55de4e9937a5cc5cadb7870e800a
pashutk <me@pashutk.ru>
2020-11-22 21:12:26 +0300

    Flo-careers: Enable sending to tg

--- flo-careers/tg.ts
+++ flo-careers/tg.ts
@@ -1,16 +1,16 @@
-import { Telegraf, Telegram } from "telegraf";
-import { TelegrafOptions } from "telegraf/typings/telegraf";
-import { TelegramOptions } from "telegraf/typings/telegram";
-import * as TE from "fp-ts/TaskEither";
-import * as C from "fp-ts/Console";
-import { TelegramError } from "telegraf/core/network/error";
+import { Telegraf, Telegram } from 'telegraf';
+import { TelegrafOptions } from 'telegraf/typings/telegraf';
+import { TelegramOptions } from 'telegraf/typings/telegram';
+import * as TE from 'fp-ts/TaskEither';
+import * as C from 'fp-ts/Console';
+import { TelegramError } from 'telegraf/core/network/error';
 import type {
   ExtraEditMessage,
   ExtraPhoto,
   InputFile,
   Message,
   MessagePhoto,
-} from "telegraf/typings/telegram-types";
+} from 'telegraf/typings/telegram-types';
 
 export const telegraf = (t: string, o?: TelegrafOptions) => new Telegraf(t, o);
 
@@ -21,11 +21,10 @@ export type ChatIdT = string | number;
 export const sendMessage = (chatId: ChatIdT, text: string, extra?: ExtraEditMessage) => (
   ti: Telegram
 ): TE.TaskEither<TelegramError, Message> =>
-  // TE.tryCatch(
-  //   () => ti.sendMessage(chatId, text, extra),
-  //   (e: TelegramError) => e
-  // );
-  TE.fromIO(C.log(`chaatId: ${chatId}; text: ${text}`)) as TE.TaskEither<TelegramError, Message>;
+  TE.tryCatch(
+    () => ti.sendMessage(chatId, text, extra),
+    (e: TelegramError) => e
+  );
 
 export const sendPhoto = (chatId: ChatIdT, photo: InputFile, extra?: ExtraPhoto) => (
   ti: Telegram
