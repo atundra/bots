582dee21ef03fc2214588492400a456a6838d5fa
pashutk <me@pashutk.ru>
2021-02-25 01:44:52 +0300

    Common: Add Hour codec

---
+++ packages/common/io-ts-types/hour.ts
@@ -0,0 +1,23 @@
+import * as t from 'io-ts';
+
+interface Bounded023Brand {
+  readonly Bounded023: unique symbol;
+}
+
+type Bounded023 = t.Branded<number, Bounded023Brand>;
+
+interface Bounded023C extends t.Type<Bounded023, number, unknown> {}
+
+/**
+ * A codec that succeeds if a number is 0-23
+ */
+const Bounded023: Bounded023C = t.brand(
+  t.number,
+  (n): n is Bounded023 => n >= 0 && n <= 24,
+  'Bounded023'
+);
+
+export const Hour = t.intersection([t.Int, Bounded023]);
+
+const h = Hour.decode(1.2);
+console.log(h);
