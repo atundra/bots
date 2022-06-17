e5b7b3ca73114ed68901cf50b4ad0e498fc68e21
pashutk <me@pashutk.ru>
2022-06-18 00:16:19 +0200

    Rotorbuilds: Typings update

--- packages/rotorbuilds/mongo.ts
+++ packages/rotorbuilds/mongo.ts
@@ -8,15 +8,15 @@ import {
   MongoClient,
   MongoError,
   OptionalId,
-} from "mongodb";
-import * as TE from "fp-ts/TaskEither";
-import * as RA from "fp-ts/lib/ReadonlyArray";
-import * as RNEA from "fp-ts/lib/ReadonlyNonEmptyArray";
-import { Branded } from "io-ts";
-import { pipe } from "fp-ts/lib/pipeable";
+} from 'mongodb';
+import * as TE from 'fp-ts/TaskEither';
+import * as RA from 'fp-ts/lib/ReadonlyArray';
+import * as RNEA from 'fp-ts/lib/ReadonlyNonEmptyArray';
+import { Branded } from 'io-ts';
+import { pipe } from 'fp-ts/lib/pipeable';
 
-type NonConnectedMongoClient = Branded<MongoClient, "nonconnected">;
-type ConnectedMongoClient = Branded<MongoClient, "connected">;
+type NonConnectedMongoClient = Branded<MongoClient, 'nonconnected'>;
+type ConnectedMongoClient = Branded<MongoClient, 'connected'>;
 
 export const client = (uri: string) =>
   new MongoClient(uri, {
@@ -26,47 +26,54 @@ export const client = (uri: string) =>
   }) as NonConnectedMongoClient;
 
 export const connect = (
-  client: NonConnectedMongoClient
+  client: NonConnectedMongoClient,
 ): TE.TaskEither<MongoError, ConnectedMongoClient> =>
   TE.tryCatch(
     () => client.connect() as Promise<ConnectedMongoClient>,
-    (err: MongoError) => err
+    (err: unknown) => err as MongoError,
   );
 
-export const db = (name: string) => (client: ConnectedMongoClient): Db => client.db(name);
+export const db =
+  (name: string) =>
+  (client: ConnectedMongoClient): Db =>
+    client.db(name);
 
-export const collection = <A>(name: string) => (db: Db) => db.collection<A>(name);
+export const collection =
+  <A>(name: string) =>
+  (db: Db) =>
+    db.collection<A>(name);
 
 export const close = (client: ConnectedMongoClient): TE.TaskEither<MongoError, void> =>
   TE.tryCatch(
     () => client.close(),
-    (err: MongoError) => err
+    (err: unknown) => err as MongoError,
   );
 
-export const useMongo = (uri: string) => <A>(
-  use: (a: ConnectedMongoClient) => TE.TaskEither<Error, A>
-): TE.TaskEither<Error, A> =>
-  TE.bracket(pipe(uri, client, connect), use, (client) => close(client));
+export const useMongo =
+  (uri: string) =>
+  <A>(use: (a: ConnectedMongoClient) => TE.TaskEither<Error, A>): TE.TaskEither<Error, A> =>
+    TE.bracket(pipe(uri, client, connect), use, (client) => close(client));
 
-export const find = <A>(q: FilterQuery<A>, o?: FindOneOptions<A extends A ? A : A>) => (
-  c: Collection<A>
-): TE.TaskEither<MongoError, A[]> =>
-  TE.tryCatch(
-    () => c.find(q, o).toArray(),
-    (e: MongoError) => e
-  );
+export const find =
+  <A>(q: FilterQuery<A>, o?: FindOneOptions<A extends A ? A : A>) =>
+  (c: Collection<A>): TE.TaskEither<MongoError, A[]> =>
+    TE.tryCatch(
+      () => c.find(q, o!).toArray(),
+      (e: unknown) => e as MongoError,
+    );
 
-export const insertMany = <A>(
-  ds: RNEA.ReadonlyNonEmptyArray<OptionalId<A>>,
-  o?: CollectionInsertManyOptions
-) => (c: Collection<A>) =>
-  TE.tryCatch(
-    () => c.insertMany(RA.toArray(ds), o),
-    (e: MongoError) => e
-  );
+export const insertMany =
+  <A>(ds: RNEA.ReadonlyNonEmptyArray<OptionalId<A>>, o?: CollectionInsertManyOptions) =>
+  (c: Collection<A>) =>
+    TE.tryCatch(
+      () => c.insertMany(RA.toArray(ds), o),
+      (e: unknown) => e as MongoError,
+    );
 
-export const deleteMany = <A>(q: FilterQuery<A>, o?: CommonOptions) => (c: Collection<A>) =>
-  TE.tryCatch(
-    () => c.deleteMany(q, o),
-    (e: MongoError) => e
-  );
+export const deleteMany =
+  <A>(q: FilterQuery<A>, o?: CommonOptions) =>
+  (c: Collection<A>) =>
+    TE.tryCatch(
+      () => c.deleteMany(q, o),
+      (e: unknown) => e as MongoError,
+    );
--- packages/rotorbuilds/tg.ts
+++ packages/rotorbuilds/tg.ts
@@ -2,7 +2,6 @@ import { Telegraf, Telegram } from 'telegraf';
 import { TelegrafOptions } from 'telegraf/typings/telegraf';
 import { TelegramOptions } from 'telegraf/typings/telegram';
 import * as TE from 'fp-ts/TaskEither';
-import { TelegramError } from 'telegraf/core/network/error';
 import type {
   ExtraEditMessage,
   ExtraPhoto,
@@ -17,18 +16,18 @@ export const telegram = (t: string, o?: TelegramOptions) => new Telegram(t, o);
 
 export type ChatIdT = string | number;
 
-export const sendMessage = (chatId: ChatIdT, text: string, extra?: ExtraEditMessage) => (
-  ti: Telegram
-): TE.TaskEither<TelegramError, Message> =>
-  TE.tryCatch(
-    () => ti.sendMessage(chatId, text, extra),
-    (e: TelegramError) => e
-  );
+export const sendMessage =
+  (chatId: ChatIdT, text: string, extra?: ExtraEditMessage) =>
+  (ti: Telegram): TE.TaskEither<Error, Message> =>
+    TE.tryCatch(
+      () => ti.sendMessage(chatId, text, extra),
+      (e: unknown) => e as Error,
+    );
 
-export const sendPhoto = (chatId: ChatIdT, photo: InputFile, extra?: ExtraPhoto) => (
-  ti: Telegram
-): TE.TaskEither<TelegramError, MessagePhoto> =>
-  TE.tryCatch(
-    () => ti.sendPhoto(chatId, photo, extra),
-    (e: TelegramError) => e
-  );
+export const sendPhoto =
+  (chatId: ChatIdT, photo: InputFile, extra?: ExtraPhoto) =>
+  (ti: Telegram): TE.TaskEither<Error, MessagePhoto> =>
+    TE.tryCatch(
+      () => ti.sendPhoto(chatId, photo, extra),
+      (e: unknown) => e as Error,
+    );
